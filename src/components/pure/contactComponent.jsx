d9946b36bd2a99f1a53aa5c671076d21cf03e3b3
danielDeVita <danielitodevita@gmail.com>
2022-10-22 18:37:49 -0300

    handler added

--- src/components/pure/contactComponent.jsx
+++ src/components/pure/contactComponent.jsx
@@ -6,15 +6,17 @@ import { Contact } from '../../models/contact.class';
 const ContactComponent = ({ contact }) => {
 
     const [conectado, setConectado] = useState(contact.conectado);
+    const handlerConectado = () => {
+        setConectado(!conectado)
+    }
 
     return (
         <div>
             <h1>Nombre: {contact.nombre} </h1>
             <h2>Apellido: {contact.apellido} </h2>
             <h3>Email: {contact.email} </h3>
-            {/* <h4>Conectado: {contact.conectado ? 'Contacto en línea' : 'Contacto no disponible'}</h4> */}
             <h4> {conectado === false ? 'Contacto no disponible' : 'Contacto En linea'}</h4>
-            <button onClick={() => setConectado(!conectado)}>{conectado === false ? 'Conectar' : 'Desconectar'}</button>
+            <button onClick={handlerConectado}>{conectado === false ? 'Conectar' : 'Desconectar'}</button>
         </div>
     );
 };
